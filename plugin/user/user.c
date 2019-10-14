296bf0ebe983ed735d6cef643e5d4e9f13520408
SKGleba <30833773+SKGleba@users.noreply.github.com>
2019-10-14 13:26:41 +0200

    v2.0

---
+++ plugin/user/user.c
@@ -0,0 +1,207 @@
+#include <psp2/kernel/clib.h>
+#include <psp2/kernel/modulemgr.h>
+#include <psp2/io/fcntl.h>
+#include <psp2/io/stat.h>
+#include <psp2/registrymgr.h>
+#include <psp2/system_param.h>
+#include <psp2/power.h>
+#include <taihen.h>
+#include <string.h>
+
+#define CONFIG_PATH "ur0:tai/cbs_cfg.bin"
+
+#define LOG(...) \
+	do { \
+		char buffer[256]; \
+		sceClibSnprintf(buffer, sizeof(buffer), ##__VA_ARGS__); \
+		logg(buffer, sceClibStrnlen(buffer, sizeof(buffer)), "ur0:temp/xmldmp.raw", 2); \
+} while (0)
+	
+#define LOG_START(...) \
+	do { \
+		char buffer[256]; \
+		sceClibSnprintf(buffer, sizeof(buffer), ##__VA_ARGS__); \
+		logg(buffer, sceClibStrnlen(buffer, sizeof(buffer)), "ur0:temp/xmldmp.raw", 1); \
+} while (0)
+
+static char cfg[4] = {0x30, 0x30, 0x30, 0}; // IMGTYPE, ION, DELAY, DELAYTIMESEC
+
+extern unsigned char _binary_theme_settings_xml_start;
+extern unsigned char _binary_theme_settings_xml_size;
+
+static SceUID g_hooks[6];
+
+static int logg(void *buffer, int length, const char* logloc, int create)
+{
+	int fd;
+	if (create == 0) {
+		fd = sceIoOpen(logloc, SCE_O_WRONLY | SCE_O_APPEND, 6);
+	} else if (create == 1) {
+		fd = sceIoOpen(logloc, SCE_O_WRONLY | SCE_O_TRUNC | SCE_O_CREAT, 6);
+	} else if (create == 2) {
+		fd = sceIoOpen(logloc, SCE_O_WRONLY | SCE_O_APPEND | SCE_O_CREAT, 6);
+	}
+	if (fd < 0)
+		return 0;
+
+	sceIoWrite(fd, buffer, length);
+	sceIoClose(fd);
+	return 1;
+}
+
+static void save_config_user(void) {
+  SceUID fd;
+  fd = sceIoOpen(CONFIG_PATH, SCE_O_TRUNC | SCE_O_CREAT | SCE_O_WRONLY, 6);
+  if (fd >= 0) {
+    sceIoWrite(fd, &cfg, sizeof(cfg));
+    sceIoClose(fd);
+  }
+}
+
+static int load_config_user(void) {
+  SceUID fd;
+  fd = sceIoOpen(CONFIG_PATH, SCE_O_RDONLY, 0);
+  if (fd >= 0) {
+    sceIoRead(fd, &cfg, sizeof(cfg));
+    sceIoClose(fd);
+	return 0;
+  }
+  save_config_user();
+  return 0;
+}
+
+static tai_hook_ref_t g_sceRegMgrGetKeyInt_SceSystemSettingsCore_hook;
+static int sceRegMgrGetKeyInt_SceSystemSettingsCore_patched(const char *category, const char *name, int *value) {
+  if (sceClibStrncmp(category, "/CONFIG/CBSM", 12) == 0) {
+    if (value) {
+      load_config_user();
+      if (sceClibStrncmp(name, "imgt", 4) == 0) {
+        *value = (cfg[0] - 0x30);
+      } else if (sceClibStrncmp(name, "ion", 3) == 0) {
+		*value = (cfg[1] - 0x30);
+	  } else if (sceClibStrncmp(name, "dfl", 3) == 0) {
+		*value = (cfg[2] - 0x30);
+	  } else if (sceClibStrncmp(name, "deltime", 7) == 0) {
+		*value = cfg[3];
+      }
+    }
+    return 0;
+  }
+  return TAI_CONTINUE(int, g_sceRegMgrGetKeyInt_SceSystemSettingsCore_hook, category, name, value);
+}
+
+static tai_hook_ref_t g_sceRegMgrSetKeyInt_SceSystemSettingsCore_hook;
+static int sceRegMgrSetKeyInt_SceSystemSettingsCore_patched(const char *category, const char *name, int value) {
+  if (sceClibStrncmp(category, "/CONFIG/CBSM", 12) == 0) {
+    if (sceClibStrncmp(name, "imgt", 4) == 0) {
+		cfg[0] = (value + 0x30);
+    } else if (sceClibStrncmp(name, "ion", 3) == 0) {
+		cfg[1] = (value + 0x30);
+	} else if (sceClibStrncmp(name, "dfl", 3) == 0) {
+		cfg[2] = (value + 0x30);
+	} else if (sceClibStrncmp(name, "deltime", 7) == 0) {
+		cfg[3] = value;
+    }
+    save_config_user();
+    return 0;
+  }
+  return TAI_CONTINUE(int, g_sceRegMgrSetKeyInt_SceSystemSettingsCore_hook, category, name, value);
+}
+
+typedef struct {
+  int size;
+  const char *name;
+  int type;
+  int unk;
+} SceRegMgrKeysInfo;
+
+static tai_hook_ref_t g_sceRegMgrGetKeysInfo_SceSystemSettingsCore_hook;
+static int sceRegMgrGetKeysInfo_SceSystemSettingsCore_patched(const char *category, SceRegMgrKeysInfo *info, int unk) {
+  if (sceClibStrncmp(category, "/CONFIG/CBSM", 12) == 0) {
+    if (info) {
+        info->type = 0x00040000; // type integer
+    }
+    return 0;
+  }
+  return TAI_CONTINUE(int, g_sceRegMgrGetKeysInfo_SceSystemSettingsCore_hook, category, info, unk);
+}
+
+static tai_hook_ref_t g_scePafMiscLoadXmlLayout_SceSettings_hook;
+static int scePafMiscLoadXmlLayout_SceSettings_patched(int a1, void *xml_buf, int xml_size, int a4) {
+  if (sceClibStrncmp(xml_buf+82, "theme_settings_plugin", 21) == 0) {
+    xml_buf = (void *)&_binary_theme_settings_xml_start;
+    xml_size = (int)&_binary_theme_settings_xml_size;
+  }
+  return TAI_CONTINUE(int, g_scePafMiscLoadXmlLayout_SceSettings_hook, a1, xml_buf, xml_size, a4);
+}
+
+static SceUID g_system_settings_core_modid = -1;
+static tai_hook_ref_t g_sceKernelLoadStartModule_SceSettings_hook;
+static SceUID sceKernelLoadStartModule_SceSettings_patched(char *path, SceSize args, void *argp, int flags, SceKernelLMOption *option, int *status) {
+  SceUID ret = TAI_CONTINUE(SceUID, g_sceKernelLoadStartModule_SceSettings_hook, path, args, argp, flags, option, status);
+  if (ret >= 0 && sceClibStrncmp(path, "vs0:app/NPXS10015/system_settings_core.suprx", 44) == 0) {
+    g_system_settings_core_modid = ret;
+    g_hooks[2] = taiHookFunctionImport(&g_scePafMiscLoadXmlLayout_SceSettings_hook, 
+                                        "SceSettings", 
+                                        0x3D643CE8, // ScePafMisc
+                                        0x19FE55A8, 
+                                        scePafMiscLoadXmlLayout_SceSettings_patched);
+    g_hooks[3] = taiHookFunctionImport(&g_sceRegMgrGetKeyInt_SceSystemSettingsCore_hook, 
+                                        "SceSystemSettingsCore", 
+                                        0xC436F916, // SceRegMgr
+                                        0x16DDF3DC, 
+                                        sceRegMgrGetKeyInt_SceSystemSettingsCore_patched);
+    g_hooks[4] = taiHookFunctionImport(&g_sceRegMgrSetKeyInt_SceSystemSettingsCore_hook, 
+                                        "SceSystemSettingsCore", 
+                                        0xC436F916, // SceRegMgr
+                                        0xD72EA399, 
+                                        sceRegMgrSetKeyInt_SceSystemSettingsCore_patched);
+    g_hooks[5] = taiHookFunctionImport(&g_sceRegMgrGetKeysInfo_SceSystemSettingsCore_hook, 
+                                        "SceSystemSettingsCore", 
+                                        0xC436F916, // SceRegMgr
+                                        0x58421DD1, 
+                                        sceRegMgrGetKeysInfo_SceSystemSettingsCore_patched);
+  }
+  return ret;
+}
+
+static tai_hook_ref_t g_sceKernelStopUnloadModule_SceSettings_hook;
+static int sceKernelStopUnloadModule_SceSettings_patched(SceUID modid, SceSize args, void *argp, int flags, SceKernelULMOption *option, int *status) {
+  if (modid == g_system_settings_core_modid) {
+    g_system_settings_core_modid = -1;
+    if (g_hooks[2] >= 0) taiHookRelease(g_hooks[2], g_scePafMiscLoadXmlLayout_SceSettings_hook);
+    if (g_hooks[3] >= 0) taiHookRelease(g_hooks[3], g_sceRegMgrGetKeyInt_SceSystemSettingsCore_hook);
+    if (g_hooks[4] >= 0) taiHookRelease(g_hooks[4], g_sceRegMgrSetKeyInt_SceSystemSettingsCore_hook);
+    if (g_hooks[5] >= 0) taiHookRelease(g_hooks[5], g_sceRegMgrGetKeysInfo_SceSystemSettingsCore_hook);
+  }
+  return TAI_CONTINUE(int, g_sceKernelStopUnloadModule_SceSettings_hook, modid, args, argp, flags, option, status);
+}
+
+void _start() __attribute__ ((weak, alias ("module_start")));
+int module_start(SceSize argc, const void *args) {
+	LOG_START("started");
+  sceClibMemset(&cfg, 0, sizeof(cfg));
+  load_config_user();
+  g_hooks[0] = taiHookFunctionImport(&g_sceKernelLoadStartModule_SceSettings_hook, 
+                                      "SceSettings", 
+                                      0xCAE9ACE6, // SceLibKernel
+                                      0x2DCC4AFA, 
+                                      sceKernelLoadStartModule_SceSettings_patched);
+  g_hooks[1] = taiHookFunctionImport(&g_sceKernelStopUnloadModule_SceSettings_hook, 
+                                      "SceSettings", 
+                                      0xCAE9ACE6, // SceLibKernel
+                                      0x2415F8A4, 
+                                      sceKernelStopUnloadModule_SceSettings_patched);
+  return SCE_KERNEL_START_SUCCESS;
+}
+
+int module_stop(SceSize argc, const void *args) {
+  // free hooks that didn't fail
+  if (g_hooks[0] >= 0) taiHookRelease(g_hooks[0], g_sceKernelLoadStartModule_SceSettings_hook);
+  if (g_hooks[1] >= 0) taiHookRelease(g_hooks[1], g_sceKernelStopUnloadModule_SceSettings_hook);
+  if (g_hooks[2] >= 0) taiHookRelease(g_hooks[2], g_scePafMiscLoadXmlLayout_SceSettings_hook);
+  if (g_hooks[3] >= 0) taiHookRelease(g_hooks[3], g_sceRegMgrGetKeyInt_SceSystemSettingsCore_hook);
+  if (g_hooks[4] >= 0) taiHookRelease(g_hooks[4], g_sceRegMgrSetKeyInt_SceSystemSettingsCore_hook);
+  if (g_hooks[5] >= 0) taiHookRelease(g_hooks[5], g_sceRegMgrGetKeysInfo_SceSystemSettingsCore_hook);
+  return SCE_KERNEL_STOP_SUCCESS;
+}
--- src/main.c
+++ src/main.c
@@ -60,6 +60,28 @@ int fcp(const char *from, const char *to) {
 	return 1;
 }
 
+int fap(const char *from, const char *to) {
+	long psz;
+	FILE *fp = fopen(from,"rb");
+
+	fseek(fp, 0, SEEK_END);
+	psz = ftell(fp);
+	rewind(fp);
+
+	char* pbf = (char*) malloc(sizeof(char) * psz);
+	fread(pbf, sizeof(char), (size_t)psz, fp);
+
+	FILE *pFile = fopen(to, "ab");
+	
+	for (int i = 0; i < psz; ++i) {
+			fputc(pbf[i], pFile);
+	}
+   
+	fclose(fp);
+	fclose(pFile);
+	return 1;
+}
+
 uint32_t get_key(void) {
 	static unsigned prev = 0;
 	SceCtrlData pad;
@@ -92,69 +114,84 @@ int ex(const char *fname) {
     return 0;
 }
 
-int config_util(void) {
-	psvDebugScreenClear();
-	static char uwu[4] = {0x30, 0x30, 0x30, 0};
-	printf("CBS-Mod configurator by SKGleba\n");
-	printf("\n1 - Boot Logo/Animation ?\n");
-	printf("Options:\n");
-	printf("  CROSS      Boot Animation\n");
-	printf("  SQUARE     Boot Logo\n\n");
-
-	if (get_key() == SCE_CTRL_CROSS) uwu[1] = 0x31;
-	
-	printf("\n2 - Delay boot ?\n");
-	printf("Options:\n");
-	printf("  CROSS      Disable\n");
-	printf("  SQUARE     5 sec\n");
-	printf("  TRIANGLE   10 sec\n");
-	printf("  CIRCLE     15 sec\n\n");
-
-	int kii = get_key();
-	
-	if (kii == SCE_CTRL_SQUARE) {
-		uwu[2] = 0x31;
-		uwu[3] = 5;
-	} else if (kii == SCE_CTRL_TRIANGLE) {
-		uwu[2] = 0x31;
-		uwu[3] = 10;
-	} else if (kii == SCE_CTRL_CIRCLE) {
-		uwu[2] = 0x31;
-		uwu[3] = 15;
-	}
-	
-	SceUID fd = sceIoOpen("ur0:tai/cbs_cfg.bin", SCE_O_WRONLY | SCE_O_TRUNC | SCE_O_CREAT, 0777);
-	sceIoWrite(fd, uwu, 4);
-	sceIoClose(fd);
-	
-	printf("done\n");
-}
-
 int main(int argc, char *argv[]) {
 	(void)argc;
 	(void)argv;
 
 	psvDebugScreenInit();
+	
+	static char version[16];
+	
+	if (ex("ur0:tai/cbs.skprx") == 1)
+		sceIoRemove("ur0:tai/cbs.skprx");
+	
+	if (ex("ur0:tai/boot_config.txt") == 0) {
+		printf("Could not find boot_config.txt in ur0:tai/ !\n");
+		sceKernelDelayThread(3 * 1000 * 1000);
+		sceKernelExitProcess(0);
+		sceKernelDelayThread(3 * 1000 * 1000);
+	}
 
-	if (ex("ur0:tai/cbs.skprx") == 0) {
-		printf("Could not find cbs.skprx in ur0:tai/ !\n");
-		if (ex("ur0:tai/custom_boot_splash.skprx") == 1) sceIoRemove("ur0:tai/custom_boot_splash.skprx");
-		printf("copying: cbs plugin... ");
-		fcp("app0:cbs.skprx", "ur0:tai/cbs.skprx");
+	if (ex("ur0:tai/cbsm.skprx") == 0) {
+		printf("Could not find cbsm.skprx in ur0:tai/ !\n");
+		printf("copying... ");
+		fcp("app0:cbsm.skprx", "ur0:tai/cbsm.skprx");
 		printf("ok!\n");
 		printf("copying: stock animation... ");
 		fcp("app0:banim.xgif", "ur0:tai/boot_animation.img");
 		printf("ok!\n");
-		printf("adding entry to ur0:tai/boot_config.txt... ");
-		SceUID fd = sceIoOpen("ur0:tai/boot_config.txt", SCE_O_WRONLY, 0777);
-		sceIoWrite(fd, (void *)"# CBS\n- load\tur0:tai/cbs.skprx\n\n# ", strlen("# CBS\n- load\tur0:tai/cbs.skprx\n\n# "));
-		sceIoClose(fd);
+		printf("copying: stock low animation... ");
+		fcp("app0:lbanim.xgif", "ur0:tai/lboot_animation.img");
 		printf("ok!\n");
+		printf("adding entry to ur0:tai/boot_config.txt... ");
+		SceUID fd = sceIoOpen("ur0:tai/boot_config_temp.txt", SCE_O_WRONLY | SCE_O_TRUNC | SCE_O_CREAT, 6);
+		sceIoWrite(fd, (void *)"# CBSM\n- load\tur0:tai/cbsm.skprx\n\n", strlen("# CBSM\n- load\tur0:tai/cbsm.skprx\n\n"));
+		sceIoClose(fd);
+		fap("ur0:tai/boot_config.txt", "ur0:tai/boot_config_temp.txt");
+		if (ex("ur0:tai/cbsm_old_cfg.txt") == 0) sceIoRename("ur0:tai/boot_config.txt", "ur0:tai/cbsm_old_cfg.txt");
+		sceIoRename("ur0:tai/boot_config_temp.txt", "ur0:tai/boot_config.txt");
+		printf("ok!\n");
+	} else {
+		printf("Found cbsm.skprx in ur0:tai/ !\n");
+		printf("removing... ");
+		sceIoRemove("ur0:tai/cbsm.skprx");
+		printf("ok!\n");
+		if (ex("ur0:tai/cbsm_old_cfg.txt") == 1) {
+			printf("restoring old boot_config.txt... ");
+			sceIoRemove("ur0:tai/boot_config.txt");
+			sceIoRename("ur0:tai/cbsm_old_cfg.txt", "ur0:tai/boot_config.txt");
+			printf("ok!\n");
+		}
 	}
 	
-	config_util();
+	if (ex("ur0:tai/cbsm.suprx") == 0) {
+		printf("Could not find cbsm.suprx in ur0:tai/ !\n");
+		printf("copying... ");
+		fcp("app0:cbsm.suprx", "ur0:tai/cbsm.suprx");
+		printf("ok!\n");
+		if (ex("ux0:tai/config.txt") == 1 && ex("ux0:tai/config_precbsm.txt") == 0) sceIoRename("ux0:tai/config.txt", "ux0:tai/config_precbsm.txt");
+		printf("adding entry to ur0:tai/config.txt... ");
+		SceUID fd = sceIoOpen("ur0:tai/config_temp.txt", SCE_O_WRONLY | SCE_O_TRUNC | SCE_O_CREAT, 6);
+		sceIoWrite(fd, (void *)"\n# CBSM\n*NPXS10015\nur0:tai/cbsm.suprx\n", strlen("\n# CBSM\n*NPXS10015\nur0:tai/cbsm.suprx\n"));
+		sceIoClose(fd);
+		fcp("ur0:tai/config.txt", "ur0:tai/config_precbsm.txt");
+		fap("ur0:tai/config_temp.txt", "ur0:tai/config.txt");
+		sceIoRemove("ur0:tai/config_temp.txt");
+		printf("ok!\n");
+	} else {
+		printf("Found cbsm.suprx in ur0:tai/ !\n");
+		printf("removing... ");
+		sceIoRemove("ur0:tai/cbsm.suprx");
+		printf("ok!\n");
+		if (ex("ur0:tai/config_precbsm.txt") == 1) {
+			printf("restoring old config.txt... ");
+			sceIoRemove("ur0:tai/config.txt");
+			sceIoRename("ur0:tai/config_precbsm.txt", "ur0:tai/config.txt");
+			printf("ok!\n");
+		}
+	}
 	
-	press_exit();
-
-	return 0;
+	printf("done!\n");
+	sceKernelDelayThread(4 * 1000 * 1000);
+	sceKernelExitProcess(0);
 }
